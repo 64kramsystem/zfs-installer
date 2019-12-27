d47102d5cf20ee47afef6e2ea2a20608abb3eadd
Saverio Miroddi <saverio.pub2@gmail.com>
2019-12-27 10:50:56 +0100

    Store log(s) into a separate directory

--- install-zfs.sh
+++ install-zfs.sh
@@ -39,6 +39,7 @@ c_default_bpool_tweaks="-o ashift=12"
 c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=lz4 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD"
 c_zfs_mount_dir=/mnt
 c_installed_os_data_mount_dir=/target
+c_log_dir="$(dirname "$(mktemp)")/zfs-installer"
 c_unpacked_subiquity_dir=/tmp/ubiquity_snap_files
 declare -A c_supported_linux_distributions=([Ubuntu]=18.04 [UbuntuServer]=18.04 [LinuxMint]=19 [Debian]=10 [elementary]=5.1)
 c_temporary_volume_size=12G  # large enough; Debian, for example, takes ~8 GiB.
@@ -166,7 +167,9 @@ When installing the O/S via $ZFS_OS_INSTALLATION_SCRIPT, the root pool is mounte
 function activate_debug {
   print_step_info_header
 
-  exec 5> "$(dirname "$(mktemp)")/install-zfs.log"
+  mkdir -p "$c_log_dir"
+
+  exec 5> "$c_log_dir/install.log"
   BASH_XTRACEFD="5"
   set -x
 }

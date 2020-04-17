ed8e6f787eb35d4af2c9dc598ce5827f57ae7e2e
Sergey Zapunidi <zapunidy@gmail.com>
2020-04-17 23:16:14 +0300

    More than 2 disks create raidz zpool instead of mirror now.

--- install-zfs.sh
+++ install-zfs.sh
@@ -332,7 +332,7 @@ function select_disks {
         menu_entries_option+=("$disk_id" "($block_device_basename)" "$disk_selection_status")
       done
 
-      local dialog_message="Select the ZFS devices (multiple selections will be in mirror).
+      local dialog_message="Select the ZFS devices (couple devices would create a mirror, more than two selections will be in raidz).
 
 Devices with mounted partitions, cdroms, and removable devices are not displayed!
 "
@@ -629,10 +629,12 @@ function prepare_disks {
     bpool_disks_partitions+=("${selected_disk}-part2")
   done
 
-  if [[ ${#v_selected_disks[@]} -gt 1 ]]; then
-    local pools_mirror_option=mirror
+  if [[ ${#v_selected_disks[@]} -gt 2 ]]; then
+    local pools_raid_option=raidz
+  elif [[ ${#v_selected_disks[@]} -eq 2 ]]; then
+    local pools_raid_option=mirror
   else
-    local pools_mirror_option=
+    local pools_raid_option=
   fi
 
   # POOLS CREATION #####################
@@ -650,7 +652,7 @@ function prepare_disks {
     "${encryption_options[@]}" \
     $v_rpool_tweaks \
     -O devices=off -O mountpoint=/ -R "$c_zfs_mount_dir" -f \
-    "$v_rpool_name" $pools_mirror_option "${rpool_disks_partitions[@]}"
+    "$v_rpool_name" $pools_raid_option "${rpool_disks_partitions[@]}"
 
   # `-d` disable all the pool features (not used here);
   #
@@ -658,7 +660,7 @@ function prepare_disks {
   zpool create \
     $v_bpool_tweaks \
     -O devices=off -O mountpoint=/boot -R "$c_zfs_mount_dir" -f \
-    "$v_bpool_name" $pools_mirror_option "${bpool_disks_partitions[@]}"
+    "$v_bpool_name" $pools_raid_option "${bpool_disks_partitions[@]}"
 
   # SWAP ###############################
 

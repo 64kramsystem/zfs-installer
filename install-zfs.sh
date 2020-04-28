8f76b4a47d1a01c10ba576053cc1cdd0289c32df
Sergey Zapunidi <zapunidy@gmail.com>
2020-04-28 12:07:43 +0300

    Fixed missing efibootmgr deb packet outside jail

--- install-zfs.sh
+++ install-zfs.sh
@@ -518,7 +518,7 @@ function install_host_packages_Debian {
     echo "deb http://deb.debian.org/debian buster-backports main contrib" >> /etc/apt/sources.list
     apt update
 
-    apt install --yes -t buster-backports zfs-dkms
+    apt install --yes -t buster-backports zfs-dkms efibootmgr
 
     modprobe zfs
   fi

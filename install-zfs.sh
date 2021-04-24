759f6067ee4d733bcb143993804775f7454c0e9c
Saverio Miroddi <saverio.pub2@gmail.com>
2021-04-24 19:18:10 +0200

    Update pool creation features to updated procedure

--- install-zfs.sh
+++ install-zfs.sh
@@ -52,7 +52,24 @@ c_memory_warning_limit=$((3584 - 128)) # megabytes; exclude some RAM, which can 
 c_default_bpool_create_options=(
   -o ashift=12
   -o autotrim=on
+  -d
+  -o feature@async_destroy=enabled
+  -o feature@bookmarks=enabled
+  -o feature@embedded_data=enabled
+  -o feature@empty_bpobj=enabled
+  -o feature@enabled_txg=enabled
+  -o feature@extensible_dataset=enabled
+  -o feature@filesystem_limits=enabled
+  -o feature@hole_birth=enabled
+  -o feature@large_blocks=enabled
+  -o feature@lz4_compress=enabled
+  -o feature@spacemap_histogram=enabled
+  -O acltype=posixacl
+  -O compression=lz4
   -O devices=off
+  -O normalization=formD
+  -O relatime=on
+  -O xattr=sa
 )
 c_default_rpool_create_options=(
   -o ashift=12

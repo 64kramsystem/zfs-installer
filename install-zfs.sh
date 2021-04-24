55acc65a13eb8b26421e4e0bbb32a023f16f8a65
Saverio Miroddi <saverio.pub2@gmail.com>
2021-04-24 13:09:53 +0200

    Add ZFS_USE_PPA option

--- install-zfs.sh
+++ install-zfs.sh
@@ -166,6 +166,7 @@ This script needs to be run with admin permissions, from a Live CD.
 The procedure can be entirely automated via environment variables:
 
 - ZFS_OS_INSTALLATION_SCRIPT : path of a script to execute instead of Ubiquity (see dedicated section below)
+- ZFS_USE_PPA                : set to 1 to use packages from `ppa:jonathonf/zfs` (automatically set to true if the O/S version doesn'\''t ship at least v0.8)
 - ZFS_SELECTED_DISKS         : full path of the devices to create the pool on, comma-separated
 - ZFS_BOOT_PARTITION_SIZE    : integer number with `M` or `G` suffix (defaults to `'${c_default_boot_partition_size}M'`)
 - ZFS_PASSPHRASE             : set non-blank to encrypt the pool, and blank not to. if unset, it will be asked.
@@ -373,7 +374,7 @@ function set_zfs_ppa_requirement {
 
   # Test returns true if $zfs_package_version is blank.
   #
-  if dpkg --compare-versions "$zfs_package_version" lt 0.8; then
+  if [[ ${ZFS_USE_PPA:-} == "1" ]] || dpkg --compare-versions "$zfs_package_version" lt 0.8; then
     v_use_ppa=1
   fi
 }
